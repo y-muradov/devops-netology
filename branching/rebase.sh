e1d03233d16bcc0c8dc62a1c8073150249920ee2
Yuriy Muradov <hello@top-prof.ru>
2022-06-13 20:36:20 +0300

    merge3

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,12 +3,6 @@
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
     echo "Next parameter: $param"
-=======
-    echo "Parameter: $param"
->>>>>>> 58e7550 (git-rebase 1)
-    count=$(( $count + 1 ))
-done
-
+echo "\$@ Parameter #$count = $param"
 echo "====="
